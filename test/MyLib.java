14718b54ae769566dbeed5c610c6e8ee2398993e
Xùdōng Yáng <wyverald@gmail.com>
2024-10-30 17:34:33 -0400

    add http_jar

---
+++ test/MyLib.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+/** A simple library for the http_jar test. */
+public class MyLib {
+  public static String myStr() {
+    return "my_string";
+  }
+}
+
--- test/repo/setup.sh
+++ test/repo/setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 cd ../../
-bazel build //distro:all
+bazel build //distro:all //test:my_jar
 cp -f bazel-bin/distro/rules_java-*.tar.gz /tmp/rules_java-HEAD.tar.gz
+cp -f bazel-bin/test/libmy_jar.jar /tmp/my_jar.jar
+
