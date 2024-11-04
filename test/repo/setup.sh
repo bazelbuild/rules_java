440e6258d6faf7fc22ea5d4c988db2f3cc3cf01b
Xùdōng Yáng <wyverald@gmail.com>
2024-11-04 13:57:23 -0500

    move jar to testdata to make integration test on 6.x happy

--- test/repo/setup.sh
+++ test/repo/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd ../../
-bazel build //distro:all //test:my_jar
+bazel build //distro:all //test/testdata:my_jar
 cp -f bazel-bin/distro/rules_java-*.tar.gz /tmp/rules_java-HEAD.tar.gz
-cp -f bazel-bin/test/libmy_jar.jar /tmp/my_jar.jar
+cp -f bazel-bin/test/testdata/libmy_jar.jar /tmp/my_jar.jar
 
