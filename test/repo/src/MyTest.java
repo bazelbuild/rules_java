bc4a03fe5525123c04a9fe879dc2882d31e7d690
Fabian Meumertzheim <fabian@meumertzhe.im>
2024-11-19 08:53:58 -0800

    Add runfiles library to release archive
    Copybara Import from https://github.com/bazelbuild/rules_java/pull/242
    
    BEGIN_PUBLIC
    Add runfiles library to release archive (#242)
    
    Closes #242
    END_PUBLIC
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/bazelbuild/rules_java/pull/242 from fmeum:fix-runfiles dcc8004733fe5a6257f5ef2fdec5b04cdb62564a
    PiperOrigin-RevId: 698035007
    Change-Id: I069b7f289a40550ac1c8024d013863e3d384d5a6

--- test/repo/src/MyTest.java
+++ test/repo/src/MyTest.java
@@ -1,15 +1,29 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import com.google.devtools.build.runfiles.AutoBazelRepository;
+import com.google.devtools.build.runfiles.Runfiles;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import mypackage.MyLib;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
+@AutoBazelRepository
 public class MyTest {
   @Test
   public void main() {
     assertEquals(MyLib.myStr(), "my_string");
   }
-}
 
+  @Test
+  public void runfiles() throws IOException {
+    Runfiles runfiles = Runfiles.preload().withSourceRepository(AutoBazelRepository_MyTest.NAME);
+    Path path = Paths.get(runfiles.rlocation("integration_test_repo/src/data.txt"));
+    assertTrue(Files.exists(path));
+  }
+}
